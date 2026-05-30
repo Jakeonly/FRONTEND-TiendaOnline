73d7aeea1d9e0a01d1fa1c25723b34d81011c665
Cristian <Jakeonly0825@gmail.com>
2026-05-30 00:17:44 -0500

    feat: add Firebase configuration files and update environment settings

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,9 +1,7 @@
-const protocol = typeof window !== 'undefined' ? window.location.protocol : 'https:';
-const hostname = typeof window !== 'undefined' ? window.location.hostname : 'localhost';
-const port = 8000;
+const apiHost = 'https://backend-tiendaonline-m1bg.onrender.com';
 
 export const environment = {
   production: true,
-  apiUrl: `${protocol}//${hostname}:${port}`,
+  apiUrl: apiHost,
   authTokenStorageKey: 'tienda_access_token',
 };
--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,9 +1,7 @@
-const protocol = typeof window !== 'undefined' ? window.location.protocol : 'http:';
-const hostname = typeof window !== 'undefined' ? window.location.hostname : 'localhost';
-const port = 8000;
+const apiHost = 'http://localhost:8000';
 
 export const environment = {
   production: false,
-  apiUrl: `${protocol}//${hostname}:${port}`,
+  apiUrl: apiHost,
   authTokenStorageKey: 'tienda_access_token',
 };
